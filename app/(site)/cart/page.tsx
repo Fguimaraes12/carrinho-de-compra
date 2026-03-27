b9a91ac340df876882a751d849df51e3bc42f674
Felipe Guimarães <guimaraesfelipe6021@gmail.com>
2026-03-26 23:10:13 -0300

    feat: implement increment/decrement functions and quantity selection on product cards

--- app/(site)/cart/page.tsx
+++ app/(site)/cart/page.tsx
@@ -4,7 +4,8 @@ import { useCart } from "../../contexts/cartContext";
 import Link from "next/link";
 
 function CartPage() {
-  const { cart, removeFromCart } = useCart();
+  const { cart, removeFromCart, incrementQuantity, decrementQuantity } =
+    useCart();
 
   return (
     <div>
@@ -18,10 +19,26 @@ function CartPage() {
             <h1>{item.title}</h1>
             <p>{item.description}</p>
             <span>{item.price}</span>
-            <span className="text-yellow-300">{item.quantity}</span>
+            <div className="pt-3">
+              <button
+                onClick={() => decrementQuantity(item.id)}
+                className="text-2xl cursor-pointer"
+              >
+                -
+              </button>
+              <span className="text-yellow-300 pl-2 pr-2 text-2xl">
+                {item.quantity}
+              </span>
+              <button
+                onClick={() => incrementQuantity(item.id)}
+                className="text-2xl cursor-pointer"
+              >
+                +
+              </button>
+            </div>
             <button
               onClick={() => removeFromCart(item.id)}
-              className="p-2 text-red-500"
+              className="p-6 text-red-500"
             >
               Remover Item
             </button>
